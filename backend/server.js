ed8d2524fc171b2bb93e2f0a5cd760667ee9428a
PavelAnnor <annorpavel@gmail.com>
2026-08-04 12:42:16 -0400

    set up user schema for backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 import express from "express"
 import cors from "cors"
+import connectDB from "./conn.js";
+
+import mongoose from "mongoose";
+
+
+//importing models
+import UserModel from "./models/userModel.js"
 
 const app = express()
 const PORT = process.env.PORT || 3000;
@@ -12,21 +19,18 @@ app.use(express.json());
 app.use(
   cors({
     origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
 );
 
-app.get("/",(req,res)=>{
-
-
-    res.send("Hola")
-
-
-
+app.get("/", async (req,res)=>{
+  const response = await UserModel.find({})
+  res.send(response)
 })
 
 
 app.listen(PORT,()=>{
     console.log("Server is runnng...")
+    connectDB();
 })
