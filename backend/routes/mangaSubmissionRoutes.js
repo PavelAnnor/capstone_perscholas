f7b5e2b329b623a03ba41986fdf743d2092719a6
PavelAnnor <annorpavel@gmail.com>
2026-08-06 11:32:20 -0400

    front end design on libray page

--- backend/routes/mangaSubmissionRoutes.js
+++ backend/routes/mangaSubmissionRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createSubmission,deleteMangaPost,getUserSubmissions} from "../controllers/mangaSubmissionControllers.js"
+import { createSubmission,deleteMangaPost,getUserSubmissions,getLastRead} from "../controllers/mangaSubmissionControllers.js"
 
 
 const router = express.Router()
 
 router.route("/").post(createSubmission)
+
+router.route("/get-last-read").post(getLastRead)
 
 router.route("/getAllMangaPosts").post(getUserSubmissions)
 
--- frontend/src/util/database.js
+++ frontend/src/util/database.js
@@ -141,37 +141,37 @@ async function loadUserData(data){
 }
 
 
-// async function findLastRead(data) {
+async function findLastRead(data) {
 
 
-//   try {
+  try {
 
-//     const x = await fetch(
-//       `http://localhost:3000/manga-submission/getAllMangaPosts`,
-//       {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(data),
-//       },
-//     );
+    const x = await fetch(
+      `http://localhost:3000/manga-submission/get-last-read`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+    );
 
-//     //if the request was bad, return false
-//     if (!x.ok || x.length === 0) {
-//       return false;
-//     }
-//     //if the request was good, parse the response to json so i can acess what the request sent back
+    //if the request was bad, return false
+    if (!x.ok || x.length === 0) {
+      return false;
+    }
+    //if the request was good, parse the response to json so i can acess what the request sent back
 
-//     return await x.json();
+    return await x.json();
     
-//   } catch (error) {
+  } catch (error) {
 
-//     return false
+    return false
     
-//   }
+  }
   
-// }
+}
 
 
-export {createUser,loginUser,createMangaSubmission,loadUserData,deleteMangaSubmission}
+export {createUser,loginUser,createMangaSubmission,loadUserData,deleteMangaSubmission,findLastRead}
