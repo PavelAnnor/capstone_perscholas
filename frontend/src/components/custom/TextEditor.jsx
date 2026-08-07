06171d7b1069768509ff99bad1b4b0543fcf429a
PavelAnnor <annorpavel@gmail.com>
2026-08-07 02:16:54 -0400

    front end design for notes editing

--- frontend/src/components/custom/TextEditor.jsx
+++ frontend/src/components/custom/TextEditor.jsx
@@ -10,20 +10,21 @@ import "react-quill-new/dist/quill.snow.css";
 import { createNotes,getNotes } from "../../util/database.js";
 
 
-export default function TextEditor({mediaId, value = ""}) {
+
+export default function TextEditor({mediaId}) {
 
         const { user,notes,setNotes } = useContext(UserContext);
        
 
-        const [notesContent,setNotesContent] = useState({title:"", content:""})
+        const [notesContent,setNotesContent] = useState({title:"", content:"",new:true})
 
 
     
-    const mappedNotes = notes.map(n=> <p key = {n._id}className="text-center border p-1">{n.title}</p>)
+    const mappedNotes = notes.map(n=> <NotesItem data = {n} setContent={setNotesContent}></NotesItem>)
         //when new notes are created send them to the DB
         //and update the state varibale that stores notes
  async function handleCreate() {
-   console.log(contentRef.current.value);
+   
    if(notesContent.title===""){
      alert("Please Add a Title");
      return
@@ -44,14 +45,20 @@ export default function TextEditor({mediaId, value = ""}) {
  }
 
 
+ //makes patch request, updating the notes instead of creating a new one
+ async function handleSave() {
+    
+ }
+
+
  function handleTitleChange(e){
 
     setNotesContent({...notesContent,title:e.target.value})
 
  }
 
+ //React quill has a special function for on change with special paramters 
  function handleContentChange(content, delta, source, editor) {
-   
    setNotesContent({...notesContent,content:content})
  }
 
@@ -65,19 +72,30 @@ export default function TextEditor({mediaId, value = ""}) {
                 type="text"
                 className="w-full mb-5 p-3"
                 placeholder="Title"
-                
                 onChange={handleTitleChange}
+                value={notesContent.title}
               ></input>
-              <ReactQuill theme="snow" 
-              onChange={handleContentChange} />
+              <ReactQuill
+                theme="snow"
+                onChange={handleContentChange}
+                value={notesContent.content}
+              />
               <div className="flex">
-                 
+                {notesContent.new && (
                   <Button size="lg" className="mt-5" onClick={handleCreate}>
                     Create New
                   </Button>
-                
-
-               
+                )}
+                {!notesContent.new && (
+                  <Button size="lg" className="mt-5" onClick={handleSave}>
+                    Save Changes
+                  </Button>
+                )}
+                {!notesContent.new && (
+                  <Button size="lg" className="mt-5" onClick={handleSave}>
+                    Delete
+                  </Button>
+                )}
               </div>
             </div>
           </section>
@@ -85,6 +103,12 @@ export default function TextEditor({mediaId, value = ""}) {
           <section className="w-2/10 border-green-600 border-2">
             <p className="p-3 text-2xl bg-[blue] border-2 ">Saved Notes</p>
             <div className="w-full  h-[50vh] overflow-y-auto">
+              {notes.length != 0 && (
+                <div className="text-center border p-2 bg-blue-400">
+                  <p>Create New</p>
+                </div>
+              )}
+
               {mappedNotes}
             </div>
           </section>
@@ -94,15 +118,24 @@ export default function TextEditor({mediaId, value = ""}) {
 }
 
 
-function NotesItem({data,title,content}){
+function NotesItem({data,setContent}){
 
     const {user} = useContext(UserContext)
 
+    // a fucntion to se the current contents of the text editor to the contents of the notes item you clicked on
+     function handleClick(){
 
-    return(
+        console.log("hello")
 
-        <p></p>
-    )
+        setContent({title:data.title,content:data.content,new:false})
+        
+     }
+
+    return (
+      <div className="text-center border p-2" onClick={handleClick} >
+        <p>{data.title}</p>
+      </div>
+    );
 
 
 }
