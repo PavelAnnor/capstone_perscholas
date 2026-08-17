2bb4345ed97ae1b6ada1f68f62b491975c6c3209
PavelAnnor <annorpavel@gmail.com>
2026-08-17 17:29:02 -0400

    deployed backend on render

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://capstone-perscholas-backend.onrender.com/",
+];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
